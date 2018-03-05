1e2f43c1abf763fe87a57e72668ee79e2b3ac492
Yusif Aliyev <yusif.projects@gmail.com>
2018-03-05 06:55:11 +0400

    Changed the color of the the tab bar by changing its background image to an empty image, And also removed that line on the bottom of tab bar bu setting its shadow image to an empty image.

--- Customizing Navigation Controller/Controller/First_VC.swift
+++ Customizing Navigation Controller/Controller/First_VC.swift
@@ -6,5 +6,8 @@ import UIKit
 class First_VC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
     }
 }
