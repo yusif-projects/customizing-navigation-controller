ed1380edff2d67b44df9e4e0783a7f4e9e750e13
Yusif Aliyev <yusif.projects@gmail.com>
2018-03-06 03:48:48 +0400

    Added title view using code. Created 2 labels with different fonts. Created a stack view using code. Put the labels into the stack view.

--- Customizing Navigation Controller/Controller/First_VC.swift
+++ Customizing Navigation Controller/Controller/First_VC.swift
@@ -9,5 +9,26 @@ class First_VC: UIViewController {
         
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationController?.navigationBar.shadowImage = UIImage()
+        
+        let label_1 = UILabel()
+        
+        label_1.text = "First "
+        label_1.font = UIFont(name: "Lobster-Regular", size: 30)
+        label_1.textColor = .white
+        label_1.sizeToFit()
+        
+        let label_2 = UILabel()
+        
+        label_2.text = "VC"
+        label_2.font = UIFont(name: "Futura-Bold", size: 25)
+        label_2.textColor = .white
+        label_2.sizeToFit()
+        
+        let sv = UIStackView(arrangedSubviews: [label_1, label_2])
+        sv.axis = .horizontal
+        sv.frame.size.width = label_1.frame.width + label_2.frame.width
+        sv.frame.size.height = max(label_1.frame.height, label_2.frame.height)
+        
+        navigationItem.titleView = sv
     }
 }
