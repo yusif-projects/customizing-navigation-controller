548c6839622a1854e7d383a6455534b9a58fe252
Yusif Aliyev <yusif.projects@gmail.com>
2018-03-06 03:58:07 +0400

    Finished the second video.

--- Customizing Navigation Controller/Controller/First_VC.swift
+++ Customizing Navigation Controller/Controller/First_VC.swift
@@ -12,15 +12,15 @@ class First_VC: UIViewController {
         
         let label_1 = UILabel()
         
-        label_1.text = "First "
-        label_1.font = UIFont(name: "Lobster-Regular", size: 30)
+        label_1.text = "first"
+        label_1.font = UIFont(name: "Futura-Medium", size: 30)
         label_1.textColor = .white
         label_1.sizeToFit()
         
         let label_2 = UILabel()
         
         label_2.text = "VC"
-        label_2.font = UIFont(name: "Futura-Bold", size: 25)
+        label_2.font = UIFont(name: "Futura-Bold", size: 30)
         label_2.textColor = .white
         label_2.sizeToFit()
         
@@ -29,6 +29,6 @@ class First_VC: UIViewController {
         sv.frame.size.width = label_1.frame.width + label_2.frame.width
         sv.frame.size.height = max(label_1.frame.height, label_2.frame.height)
         
-        navigationItem.titleView = sv
+        //navigationItem.titleView = sv
     }
 }
--- Customizing Navigation Controller/Controller/Third_VC.swift
+++ Customizing Navigation Controller/Controller/Third_VC.swift
@@ -6,6 +6,27 @@ import UIKit
 class Third_VC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let label_1 = UILabel()
+        
+        label_1.text = "third"
+        label_1.font = UIFont(name: "Futura-Medium", size: 30)
+        label_1.textColor = .white
+        label_1.sizeToFit()
+        
+        let label_2 = UILabel()
+        
+        label_2.text = "VC"
+        label_2.font = UIFont(name: "Futura-Bold", size: 30)
+        label_2.textColor = .white
+        label_2.sizeToFit()
+        
+        let sv = UIStackView(arrangedSubviews: [label_1, label_2])
+        sv.axis = .horizontal
+        sv.frame.size.width = label_1.frame.width + label_2.frame.width
+        sv.frame.size.height = max(label_1.frame.height, label_2.frame.height)
+        
+        //navigationItem.titleView = sv
     }
 }
 
